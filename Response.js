77076d0c92adae465a5c917eeadc1c080cb4da6d
Bence Dányi <madbence@gmail.com>
2012-08-22 16:05:08 +0200

    Response wrapper improved

--- Response.js
+++ Response.js
@@ -1,12 +1,35 @@
 function Response(raw)
 {
-	raw.redirect=function(url, code)
+	var responseCode=200;
+	var headers={'Content-Type': 'text/html'};
+	var response=raw;
+	response.redirect=function(url, code)
 	{
 		code=code||302;
 		raw.writeHead(code,{'Location': url});
 		raw.end();
+	};
+	response.setResponseCode=function(code)
+	{
+		responseCode=code;
 	}
-	return raw;
+	response.setHeader=function(name, value)
+	{
+		headers[name]=value;
+	}
+	response.sendHead=function()
+	{
+		raw.writeHead(responseCode, headers);
+	}
+	response.setContentType=function(type)
+	{
+		var types={
+			'plain': 'text/plain',
+			'html': 'text/html',
+		};
+		headers['Content-Type']=types[type] === undefined ? type : types[type];
+	}
+	return response;
 }
 
 exports.Response=Response;
