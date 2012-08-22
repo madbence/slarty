459b14875dc0055cba07b9fcd72ad1d694c5e220
Bence Dányi <madbence@gmail.com>
2012-08-22 20:37:18 +0200

    Conditional template expressions, modified template rendering.

--- Template.js
+++ Template.js
@@ -1,61 +1,6 @@
 var cache={};
 var fs=require('fs');
 
-function Template(file)
-{
-	var content=fs.readFileSync('./templates/'+file, 'utf-8');
-	var template={
-		'render': function(obj)
-		{
-			var replacerFunc=function()
-			{
-				var name=arguments[1];
-				var decorator=arguments[3];
-				if(decorator === undefined)
-				{
-					if(obj[name] === undefined)
-					{
-						return '{'+name+'}';
-					}
-					if(typeof obj[name] === 'function')
-					{
-						return obj[name]();
-					}
-					return obj[name];
-				}
-				switch(decorator)
-				{
-					case 'date': 
-						var date=new Date(obj[name]);
-						return date.getFullYear()+'.'+
-							(date.getMonth()<9?'0':'')+(date.getMonth()+1)+'.'+
-							(date.getDate()<10?'0':'')+date.getDate()+'. '+
-							(date.getHours()<10?'0':'')+date.getHours()+':'+
-							(date.getMinutes()<10?'0':'')+date.getMinutes();
-					case 'time':
-						var date=obj[name];
-						return Math.floor(date/24/60/60)+':'+Math.floor(date/60/60%24)+':'+Math.floor(date/60%60)+':'+(date%60);
-					default: return obj[name];
-				}
-			}
-			if(obj instanceof Array)
-			{
-				var temp='';
-				for(var i in obj)
-				{
-					var temp2=obj;
-					var obj=obj[i];
-					temp+=content.replace(/\{([\w]*?)(\|(.*?))?\}/g, replacerFunc);
-					obj=temp2;
-				}
-				return temp;
-			}
-			return content.replace(/\{([\w]*?)(\|(.*?))?\}/g, replacerFunc);
-		}
-	};
-	return template;
-}
-
 function get(name)
 {
 	return cache[name] || (cache[name]=new Template(name));
@@ -70,3 +15,105 @@ setInterval(function()
 }, 2000);
 
 exports.get=get;
+
+function Template(name)
+{
+	var content=fs.readFileSync('./templates/'+name, 'utf-8');
+	var variables={};
+	var asArray=false;
+	var variableArray=null;
+	var renderedContent=null;
+	var template={
+		'addVariable': function(nname, value)
+		{
+			variables[nname]=value;
+			return template;
+		},
+		'addVariables': function(obj)
+		{
+			for(var i in obj)
+			{
+				template.addVariable(i, obj[i]);
+			}
+			return template;
+		},
+		'setVariableArray': function(arr)
+		{
+			variableArray=arr;
+			asArray=true;
+			return template;
+		},
+		'render': function()
+		{
+			console.log(name+' has variables: '+JSON.stringify(variables, null, 2));
+			if(asArray)
+			{
+				var temp='';
+				for(var i in variableArray)
+				{
+					template.addVariables(variableArray[i])
+					temp+=doReplace(content, variables);
+				}
+				return temp;
+			}
+			return doReplace(content, variables);
+		},
+		'toString': function()
+		{
+			return template.render();
+		}
+	}
+	return template;
+}
+
+function doReplace(str, variables)
+{
+	var replacerFunc=function()
+	{
+		var name=arguments[1];
+		var decorator=arguments[3];
+		if(decorator === undefined)
+		{
+			if(variables[name] === undefined)
+			{
+				return '{'+name+'}';
+			}
+			if(typeof variables[name] === 'function')
+			{
+				return variables[name]();
+			}
+			if(typeof variables[name].render === 'function')
+			{
+				console.log('Adding variables to '+name);
+				variables[name].addVariables(variables);
+				return variables[name].render();
+			}
+			return variables[name];
+		}
+		switch(decorator)
+		{
+			case 'date': 
+				var date=new Date(variables[name]);
+				return date.getFullYear()+'.'+
+					(date.getMonth()<9?'0':'')+(date.getMonth()+1)+'.'+
+					(date.getDate()<10?'0':'')+date.getDate()+'. '+
+					(date.getHours()<10?'0':'')+date.getHours()+':'+
+					(date.getMinutes()<10?'0':'')+date.getMinutes();
+			case 'time':
+				var date=variables[name];
+				return Math.floor(date/24/60/60)+':'+Math.floor(date/60/60%24)+':'+Math.floor(date/60%60)+':'+(date%60);
+			default: return variables[name];
+		}
+	}
+	var controlReplacerFunc=function()
+	{
+		switch(arguments[1])
+		{
+			case 'if':
+				return variables[arguments[2]] === true ? arguments[3] : '';
+			default:
+				return arguments[0];
+		}
+	}
+	return str.replace(/\{(if):(\w.*?)\}([\s\S]*?)\{\/\1:\2\}/g, controlReplacerFunc).replace(/\{([\w]*?)(\|(.*?))?\}/g, replacerFunc);
+}
